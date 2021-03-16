516e687540876f6048cb9095683e730a5f2a67df
frausafiye <s_gulenc@outlook.com>
2021-03-16 13:13:31 +0100

    Updates

---
+++ precache-manifest.4c48d3bc49edcb0b4601c1bfd5b90f2b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746a89ccaeffad4b6355d043988aed4d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "783e1cda607f6213ea2c",
+    "url": "./static/css/main.c181634b.chunk.css"
+  },
+  {
+    "revision": "99e6d447da5f244d5518",
+    "url": "./static/js/2.83db29f7.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "./static/js/2.83db29f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "783e1cda607f6213ea2c",
+    "url": "./static/js/main.9890d106.chunk.js"
+  },
+  {
+    "revision": "212a9d03eeb926748f56",
+    "url": "./static/js/runtime-main.2e9d68cf.js"
+  },
+  {
+    "revision": "2b6a7274701d3c314763906d64fa71de",
+    "url": "./static/media/about-img.2b6a7274.JPG"
+  },
+  {
+    "revision": "bcb740be297b946e1b682d4fdb676ad4",
+    "url": "./static/media/bookshopper.bcb740be.png"
+  },
+  {
+    "revision": "dc63627b70067cf0a4831665fecd1774",
+    "url": "./static/media/connect-img.dc63627b.jpg"
+  },
+  {
+    "revision": "6a727ab4399c0a10a5998eff8986c0d5",
+    "url": "./static/media/hero-img.6a727ab4.jpg"
+  },
+  {
+    "revision": "f1a2153f726bd942398e627f02d4d7ea",
+    "url": "./static/media/kita.f1a2153f.jpg"
+  },
+  {
+    "revision": "7dd3937f42f79b4ed71afa055cc4bc11",
+    "url": "./static/media/xox.7dd3937f.png"
+  }
+]);
