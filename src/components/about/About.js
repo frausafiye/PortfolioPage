9612310ec2d7c0b574450bae89f7dbff00779964
frausafiye <s_gulenc@outlook.com>
2023-06-12 14:29:36 +0200

    changed the about me text

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,17 +1,13 @@
-import React from 'react'
-import AboutImg from '../../images/hero-img.jpg'
-import './about.css'
+import React from "react";
+import AboutImg from "../../images/hero-img.jpg";
+import "./about.css";
 
 export default function About() {
   return (
-<section className="section about" id='about'>
+    <section className="section about" id="about">
       <div className="section-center about-center">
         <article className="about-img">
-          <img
-            src={AboutImg}
-            className="hero-photo"
-            alt="about img"
-          />
+          <img src={AboutImg} className="hero-photo" alt="about img" />
         </article>
         <article className="about-info">
           <div className="section-title about-title">
@@ -19,15 +15,24 @@ export default function About() {
             <div className="underline"></div>
           </div>
           <p>
-            I decided to change my career path a year ago, taking my experience of working in a team with me and beginning a new journey with Web Development. I started a 13 month coding school and became completely hooked.
+            I am at the foot of the mountains and enjoy every step I take to the
+            peaks. I regret not having discovered the beauty of these mountains
+            beforehand.{" "}
           </p>
           <p>
-            During the course, I enjoyed the challenges and the effort required to overcome them, the feeling of success that came at the end, and anticipating the new challenges to come.
-            Knowing that I will keep learning, as well as keep facing new challenges and continue to solve them, has led me to become incredibly integrated into the field of web development. Do I love my job? Yes!
+            As I climb higher, my vision becomes clearer, alternatives emerge,
+            and with the experience gained from what I left behind, each step
+            becomes stronger than the previous one, fueling my motivation even
+            more.
           </p>
-          <div className="btn-box"><a href="#skills" className="btn">my skills</a></div>
+          <p>This is my motivation as a web developer...</p>
+          <div className="btn-box">
+            <a href="#skills" className="btn">
+              see my skills
+            </a>
+          </div>
         </article>
       </div>
     </section>
-  )
+  );
 }
